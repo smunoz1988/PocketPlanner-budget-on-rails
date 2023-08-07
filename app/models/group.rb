6338caf4f7b3410db23fa99b6a092c2d00167c4c
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-07 16:54:30 -0500

    add validations to icon and name on group creation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :movements, foreign_key: 'group_id'
+
+  validates :name, presence: true, uniqueness: true
+  validates :icon, presence: true
 end
