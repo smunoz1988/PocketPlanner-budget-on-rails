99cc8c4ffd9d86ea8cbb70e2d79a61d13bf4b2cc
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-07 16:49:24 -0500

    add condition after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index], unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    user_groups_path(current_user)
+  end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-end
