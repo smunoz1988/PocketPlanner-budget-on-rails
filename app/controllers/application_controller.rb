1810daf8face0dff116b5a85ba8cc647010bc6dc
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-07 16:06:35 -0500

    linter fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :authenticate_user!, except: [:index], unless: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:index], unless: :devise_controller?
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,4 @@
 class Movement < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :group, class_name: 'Group'
+  belongs_to :author, class_name: 'User'
+  belongs_to :group, class_name: 'Group'
 end
