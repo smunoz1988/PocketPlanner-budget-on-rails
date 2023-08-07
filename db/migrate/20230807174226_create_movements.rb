54174f954c3792358f60bc882235b4c300692ab2
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-07 12:56:47 -0500

    setu up models for users and movements

---
+++ db/migrate/20230807174226_create_movements.rb
@@ -0,0 +1,10 @@
+class CreateMovements < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movements do |t|
+      t.string :name
+      t.integer :amount
+      t.references :author, null: false, foreign_key: { to_table: :users, column: :author_id }
+      t.timestamps
+    end
+  end
+end
