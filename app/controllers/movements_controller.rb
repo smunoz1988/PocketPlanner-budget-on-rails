0046349edd752957590736f5e04ff6f61ed2f949
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-08 12:22:23 -0500

    add movements to movements controller

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,4 +1,6 @@
 class MovementsController < ApplicationController
   def index
+    # Get all movements from the current user and from the current group
+    @movements = current_user.movements.where(author_id: current_user.id, group_id: params[:group_id])
   end
 end
