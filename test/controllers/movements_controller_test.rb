487dbd993ff48c8d1841b4a92b787862072e5e6a
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-08 11:43:47 -0500

    create comments index view

---
+++ test/controllers/movements_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class MovementsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get movements_index_url
+    assert_response :success
+  end
+end
