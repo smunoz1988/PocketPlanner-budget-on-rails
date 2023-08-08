320e84554c678dda782fc96469519e5b9607e476
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-07 21:33:31 -0500

    add new method on groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,10 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @groups = Group.where(author_id: current_user.id)
+    p @groups
+  end
+
+  def new
+    @group = Group.new
+  end
 end
