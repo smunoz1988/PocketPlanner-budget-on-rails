5ad321bbc3483055dd7e1a6b90926580c26d0d16
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-08 11:41:22 -0500

    add movements to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   resources :user, only: [] do
-    resources :groups, only: [:index, :new, :create]
+    resources :groups, only: [:index, :new, :create] do
+      resources :movements, only: [:index, :new, :create]
+    end
   end
 end
