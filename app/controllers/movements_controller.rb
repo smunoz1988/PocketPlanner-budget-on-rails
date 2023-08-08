43c31d2de603bb560bf4c68c22218de041874e97
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-08 15:40:12 -0500

    add total amount in group details

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,6 +1,7 @@
 class MovementsController < ApplicationController
   def index
     # Get all movements from the current user and from the current group
+    @group = Group.find(params[:group_id])
     @movements = current_user.movements.where(author_id: current_user.id, group_id: params[:group_id])
   end
 
