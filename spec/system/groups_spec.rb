1c983f8571983bc50d6f8b2a8b4d079e33c00ed9
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-09 17:05:38 -0500

    add integration test for groups

---
+++ spec/system/groups_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups Index', type: :system do
+  include Devise::Test::IntegrationHelpers
+
+  before do
+    @user = User.create(name: 'mike', email: 'mihael@gmail.com', password: '123456', password_confirmation: '123456')
+
+    login_as(@user, scope: :user)
+    driven_by(:rack_test)
+  end
+
+  it 'displays the add new category button' do
+    visit user_groups_path(@user)
+    expect(page).to have_content("add a new category")
+  end
+end
