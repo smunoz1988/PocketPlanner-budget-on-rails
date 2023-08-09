2b2ce7f2ade0ae92d469b9f53678f3f1970034fa
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-09 16:11:37 -0500

    update groups tests

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Groups', type: :request do
   include Devise::Test::IntegrationHelpers
+  
+  before(:each) do
+    @user = User.create(name: 'mike', email: 'mihael@gmail.com', password: '123456', password_confirmation: '123456')
+
+    login_as(@user, scope: :user)
+  end
+
   describe 'user_groups_path' do
     it 'returns http success' do
-      @user = User.create(name: 'mike', email: 'mihael@gmail.com', password: '123456',
-                          password_confirmation: '123456')
-
-      login_as(@user, scope: :user)
-
       get user_groups_path(@user)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success for new' do
+      get new_user_group_path(@user)
       expect(response).to have_http_status(:success)
     end
   end
